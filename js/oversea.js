25de3fcabfe40122e2a97632eca9762ad1b74e70
pcxweb <1214840825@qq.com>
2018-10-11 16:40:46 +0800

    留学修改

--- js/oversea.js
+++ js/oversea.js
@@ -1,46 +1,142 @@
 
 var countryjson = {
 		"Britain":{
+			"en_name":"United Kingdom",
+			"zh_name":"英国留学",
 			"bannertop":"Britainbg.jpg",
 			"sketch": "sketch1.jpg",
 			"case":{
 				"title":"英国",
-				"imgs": ["Britaincase1.png","Britaincase2.png","Britaincase3.png","Britaincase4.png"]
-			}
+				"imgs": ["Britaincase3.png","Britaincase4.png"],
+				"content":[
+					{
+						"title":"<p>专升本，</p><p>逆袭拿下“伯明翰城市大学”</p>",
+						"student":"某技术学院毕业<span style=\"color: red\">大专学生</span>黄同学，在校均分3.5， 申请化学工程专业",
+						"advantage":"英国申请灵活，学生在校分灵活",
+						"inferiority":"院校一般，且学生作品集准备时间不够充足",
+						"summary":"若学生的毕业学历不佳，可以多国家考虑，英国澳洲这类国家能为学生匹配到极佳的院校。"
+					},
+					{
+						"title":"<p>非211学校，</p><p>斩获世界百强“杜伦大学”offer</p>",
+						"student":"某普通高校英文系在读学生张同学，在校均分3.87<span style=\"color: red\">3.87</span>，申请室内设计专业",
+						"advantage":"学生除在校高分外无其他明显优势",
+						"inferiority":"院校普通，雅思成绩不高，学生背景材料一般",
+						"summary":"对于申请时背景非常普通的学生，可以利用优秀的文书及对于专业的理解来打动考官。"
+					}
+				]
+			},
+			"target_country":["英国"],
+			"target_education":"中学/本科/硕士",
+			"target_range":"5院校",
+			"target_cycle":"1个月 - 18个月"
 		},
 		"Hongkong":{
+			"en_name":"Hong Kong",
+			"zh_name":"香港留学",
 			"bannertop":"hkbg.jpg",
 			"sketch": "sketch2.jpg",
 			"case":{
 				"title":"香港",
-				"imgs": ["hkcase1.png","hkcase2.png","hkcase3.png","hkcase4.png"]
-			}
+				"imgs": ["hkcase3.png","hkcase4.png"],
+				"content":[
+					{
+						"title":"<p>普本升硕，</p><p>增大优势，终获 “香港城市大学”offer</p>",
+						"student":"<span style=\"color: red\">非重点高校</span>的方同学，雅思6分，在校平均分77分， 申请文化研究专业",
+						"advantage":"学生对于申请非常积极配合，文书不纠结",
+						"inferiority":"雅思成绩竞争力不强，学生只报一所大学",
+						"summary":"电商专业相对其他商科竞争力小，相对录取几率大，在文书背景中能有很好的提升，配合在职工作经验，大大提升录取机率"
+					},
+					{
+						"title":"<p>提前做规划，</p><p>斩获 “香港大学”offer</p>",
+						"student":"国内国际学校在读学生，学校预测IB成绩34分，雅思6.5分， 申请香港本科商科专业",
+						"advantage":"国际学校在读课程为国际认可的高中系统课程",
+						"inferiority":"国际学校老师提供<span style=\"color: red\">官方文件所需时间长</span>",
+						"summary":"申请本科全部都要求面试，甚至要求到香港面试，申请完毕后的面试培训提高了学生录取的可能性。"
+					}
+				]
+			},
+			"target_country":["中国香港"],
+			"target_education":"本科/硕士",
+			"target_range":"5院校",
+			"target_cycle":"3个月 - 18个月"
 		},
 		"Canada":{
+			"en_name":"Canada",
+			"zh_name":"加拿大留学",
 			"bannertop":"Canadabg.jpg",
 			"sketch": "sketch3.jpg",
 			"case":{
 				"title":"加拿大",
-				"imgs": []
-			}
+				"imgs": [],
+				"content":[]
+			},
+			"target_country":["加拿大"],
+			"target_education":"中学/本科/硕士",
+			"target_range":"5院校",
+			"target_cycle":"3个月 - 18个月"
 		},
 		"America":{
+			"en_name":"United States of America",
+			"zh_name":"美国留学",
 			"bannertop":"Americabg.jpg",
 			"sketch": "sketch4.jpg",
 			"case":{
 				"title":"美国",
-				"imgs": ["Americacase1.png","Americacase2.png","Americacase3.png","Americacase4.png"]
-			}
+				"imgs": ["Americacase3.png","Americacase4.png"],
+				"content":[
+					{
+						"title":"<p>本升硕，</p><p>低分拿下世界TOP50“波士顿大学”offer</p>",
+						"student":"某重点本科在读张同学，在校均分85，申请统计学专业，<span style=\"color: red\">托福95分</span>",
+						"advantage":"学生文笔功底强，对文书要求高",
+						"inferiority":"学生托福低，只有优秀的在校背景及个人专业能力",
+						"summary":"背景专业知识固然重要，同时也需要懂得把握好时间来提升语言成绩。提前与想要申请的院校教授取得联系也不失为一种有效直接的办法。"
+					},
+					{
+						"title":"<p>积极配合，</p><p>终圆梦“芝加哥哥伦比亚大学学院”</p>",
+						"student":"美国高中在读刘同学， ACT 25， TOEFL 91",
+						"advantage":"在校均分高，学生课外活动多",
+						"inferiority":"<span style=\"color: red\">没有国内应试考试的技巧</span>，因此语言分数无竞争力",
+						"summary":"学生如果能利用假期时间回国好好准备语言培训，则可能达到更高的分数，申请更好的名校。"
+					}
+				]
+			},
+			"target_country":["美国"],
+			"target_education":"中学/本科/硕士",
+			"target_range":"5-8院校",
+			"target_cycle":"3个月 - 18个月"
 		},
 		"Australia":{
+			"en_name":"Auatralia",
+			"zh_name":"澳大利亚留学",
+			"en_name2":"New Zealand",
+			"zh_name2":"新西兰留学",
 			"bannertop":"Australiabg.jpg",
 			"sketch": "sketch5.jpg",
 			"case":{
 				"title":"澳洲",
-				"imgs": ["Australiacase1.png","Australiacase2.png","Australiacase3.png","Australiacase4.png"]
-			}
+				"imgs": ["Australiacase3.png","Australiacase4.png"],
+				"content":[
+					{
+						"title":"<p>双非无雅思，早准备，</p><p>喜获 “悉尼大学”offer</p>",
+						"student":"某普通高校在读王同学，在校均分85，申请管理学硕士",
+						"advantage":"学生高度配合申请老师，并<span style=\"color: red\">提早1年多递交申请</span>",
+						"inferiority":"学校背景优势不强",
+						"summary":"通过很好的专业理解及提前的时间规划，虽然院校背景一般，没有雅思情况下，凭借这早申请，获得了录取。"
+					},
+					{
+						"title":"<p>硬件不够，文书来凑，</p><p>成功申请“墨尔本大学”</p>",
+						"student":"某一本院校在读刘同学，<span style=\"color: red\">在校均分不到80</span>，申请公共健康",
+						"advantage":"学生在读专业背景对口，能提供在有价值的专业理解",
+						"inferiority":"雅思只有6.5分，平均分不上80",
+						"summary":"通过很好的专业理解及提前的时间规划，可以通过文书来弥补硬件的不足。"
+					}
+				]
+			},
+			"target_country":["澳大利亚","新西兰"],
+			"target_education":"中学/本科/硕士",
+			"target_range":"5院校",
+			"target_cycle":"1个月 - 12个月"
 		}
-		
 	}
 $(function(){
 	var urlparam = getUrlArgObject();
@@ -51,64 +147,98 @@ $(function(){
 			var bannerimg =  countryjson[countrys].bannertop
 			var sketchimg = countryjson[countrys].sketch
 			$(".bannerbg img").attr("src","img/"+bannerimg);
-			$("#sketch .sketchimg").attr("src","img/"+sketchimg);
+			// 申请简述
+			var countryarr = countryjson[countrys].target_country
+			var copstr = ""
+			for (var i = 0; i < countryarr.length; i++) {
+				copstr += '<p>'+countryarr[i]+'</p>'
+			}
+			var teducation = countryjson[countrys].target_education
+			var trange = countryjson[countrys].target_range
+			var tcycle = countryjson[countrys].target_cycle
+			// 背景图
+			$("#sketch").css("backgroundImage","url('/own/web/img/"+countryjson[countrys].sketch+"')")
+			$("#sketch .target_country").html(copstr)
+			$("#sketch .target_education").text(teducation)
+			$("#sketch .target_range").text(trange)
+			$("#sketch .target_cycle").text(tcycle)
+			// 顶部banner标题
+			$(".banner_content .zh-name").text(countryjson[countrys].zh_name)
+			$(".banner_content .en-name").text(countryjson[countrys].en_name)
+			if(countryjson[countrys].en_name2){
+				$(".banner_content .zh-name2").text(countryjson[countrys].zh_name2)
+				$(".banner_content .en-name2").text(countryjson[countrys].en_name2)
+			}
+
 			var caseaobj = countryjson[countrys].case
 			var title = caseaobj.title
 			$("#case .txt-title").text(title);
-			var imgarr = caseaobj.imgs
+			var contentarr = caseaobj.content
+
 			// 经典案例
-			if(imgarr.length>0){
+			if(contentarr.length>0){
 				$("#case").css("display","block")
-				var imgstr = "";
-				for (var i = 0; i < imgarr.length; i++) {
-					imgstr += '<div class="swiper-slide"><img src="img/'+imgarr[i]+'" /></div>'
+				$(".canadaNo").css("display","inline-block")
+				var conBoxstr = "";
+				for (var i = 0; i < contentarr.length; i++) {
+					conBoxstr += '<div class="swiper-slide"><div class="con_box"><div class="c_title">'+contentarr[i].title+'</div>'
+						+ '<div class="c_center clearfix"><div class="left"><h4><span>学生背景资料</span></h4>'
+						+ '<div class="txt">'+contentarr[i].student+'</div>'
+						+ '</div><div class="right"><h4><span>申请优势和劣势</span></h4><div class="txt">'
+						+ '<p><strong>优势：</strong>'+contentarr[i].advantage+'</p><p><strong>劣势：</strong>'+contentarr[i].inferiority+'</p>'
+						+ '</div></div></div><div class="c_bottom clearfix"><div class="b_title"><span>总结</span></div>'
+						+ '<div class="txt">'+contentarr[i].summary+'</div></div></div></div>'
 				}
-				$("#casebanner .swiper-wrapper").html(imgstr)
+				$("#ceshibanner .swiper-wrapper").html(conBoxstr)
+
+				certifySwiper = new Swiper('#ceshibanner .swiper-container', {
+						watchSlidesProgress: true,
+						slidesPerView: 'auto',
+						centeredSlides: true,
+						loop: true,
+						loopedSlides: contentarr.length, //轮播的总个数
+						navigation: {
+							nextEl: '.swiper-button-next',
+							prevEl: '.swiper-button-prev',
+						},
+						on: {
+							progress: function(progress) {
+								for (i = 0; i < this.slides.length; i++) {
+									var slide = this.slides.eq(i);
+									var slideProgress = this.slides[i].progress;
+									modify = 1;
+									if (Math.abs(slideProgress) > 1) {
+										modify = (Math.abs(slideProgress) - 1) * 0.3 + 1;
+									}
+									translate = slideProgress * modify * 260 + 'px';
+									scale = 1 - Math.abs(slideProgress) / 5;
+									zIndex = 999 - Math.abs(Math.round(10 * slideProgress));
+									slide.transform('translateX(' + translate + ') scale(' + scale + ')');
+									slide.css('zIndex', zIndex);
+									slide.css('opacity', 1);
+									if (Math.abs(slideProgress) > 3) {
+										slide.css('opacity', 0);
+									}
+								}
+							},
+							setTransition: function(transition) {
+								for (var i = 0; i < this.slides.length; i++) {
+									var slide = this.slides.eq(i)
+									slide.transition(transition);
+								}
+
+							}
+						}
+					})
 			}else{
 				$("#case").css("display","none")
+				$(".canadaNo").css("display","none")
 			}
+
+			
+			
+
 		}
-		
 	}
-	certifySwiper = new Swiper('#casebanner .swiper-container', {
-		watchSlidesProgress: true,
-		slidesPerView: 'auto',
-		centeredSlides: true,
-		loop: true,
-		loopedSlides: 4,
-		autoplay: true,
-		navigation: {
-			nextEl: '.swiper-button-next',
-			prevEl: '.swiper-button-prev',
-		},
-		on: {
-			progress: function(progress) {
-				for (i = 0; i < this.slides.length; i++) {
-					var slide = this.slides.eq(i);
-					var slideProgress = this.slides[i].progress;
-					modify = 1;
-					if (Math.abs(slideProgress) > 1) {
-						modify = (Math.abs(slideProgress) - 1) * 0.3 + 1;
-					}
-					translate = slideProgress * modify * 260 + 'px';
-					scale = 1 - Math.abs(slideProgress) / 5;
-					zIndex = 999 - Math.abs(Math.round(10 * slideProgress));
-					slide.transform('translateX(' + translate + ') scale(' + scale + ')');
-					slide.css('zIndex', zIndex);
-					slide.css('opacity', 1);
-					if (Math.abs(slideProgress) > 3) {
-						slide.css('opacity', 0);
-					}
-				}
-			},
-			setTransition: function(transition) {
-				for (var i = 0; i < this.slides.length; i++) {
-					var slide = this.slides.eq(i)
-					slide.transition(transition);
-				}
-
-			}
-		}
-
-	})
+	
 })
